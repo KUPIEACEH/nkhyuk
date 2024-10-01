3668d928d1e5d1ee299aff4c5f6e4af467d76b7f
KUPIEACEH <134169988+KUPIEACEH@users.noreply.github.com>
2024-10-02 01:21:38 +0700

    Update index.tsx

--- components/BrideSection/index.tsx
+++ components/BrideSection/index.tsx
@@ -11,7 +11,7 @@ const BrideSection = () => {
           Dengan Rahmat Allah yang Maha Kuasa InsyaAllah kami akan melangsungkan pernikahan pada:
         </p>
         <h3 className="sm:text-[32px] text-[24px] text-center font-medium text-zinc-800 leading-tight my-6" data-aos="fade-down" data-aos-delay="800">
-          Minggu, 30 April 2023
+          Minggu, 30 April 2025
         </h3>
         <p className="text-zinc-600 text-center sm:text-[18px]" data-aos="fade-down" data-aos-delay="1100">
           di Kediaman Mempelai Wanita
